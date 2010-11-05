99333e61281f8bc249b8c1eaf1180fc11af21e38
Laramie Crocker <laramie@berkeley.edu>
2010-11-05 23:29:45 +0000

    CSPACE-3078 on branch sprint

---
+++ services/dimension/client/src/test/java/org/collectionspace/services/client/test/DimensionXmlReplayTest.java
@@ -0,0 +1,35 @@
+package org.collectionspace.services.client.test;
+
+import org.collectionspace.services.IntegrationTests.xmlreplay.ServiceResult;
+import org.collectionspace.services.IntegrationTests.xmlreplay.XmlReplay;
+import org.collectionspace.services.IntegrationTests.xmlreplay.XmlReplayTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+/**
+ * User: laramie
+ * $LastChangedRevision:  $
+ * $LastChangedDate:  $
+ */
+public class DimensionXmlReplayTest extends XmlReplayTest { 
+
+    @Test
+    public void runMaster() throws Exception {
+        XmlReplay replay = createXmlReplayFromIntegrationTestsModule("../../");
+        List<List<ServiceResult>> list = replay.runMaster("dimension-master.xml");
+        logTestForGroup(list, "runMaster");
+    }
+
+    @Test
+    public void runOneTest() throws Exception {
+        XmlReplay replay = createXmlReplayForModule();
+        replay.readOptionsFromMasterConfigFile("dimension-master.xml");
+        replay.setControlFileName("dimension.xml");
+
+        List<ServiceResult> list = replay.runTest("dimensionTestGroup", "dimension1");
+        logTest(list, "runOneTest");
+    }
+
+}
